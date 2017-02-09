3b936f28c83cda63ca1b1add9adc27df270d1761
dimitar-kunchev <dimitar.kunchev@racecloud.net>
2017-02-09 16:47:43 +0200

    Added output frequency control and display (using the spectrogram view)

--- AudioAnalyser/AASpectrumView.m
+++ AudioAnalyser/AASpectrumView.m
@@ -20,6 +20,8 @@
         frequencyLines = [NSArray arrayWithObjects:@(20), @(50), @(100), @(200), @(500), @(1000), @(2000), @(5000), @(10000), @(20000), nil];
         
         [self performSelectorInBackground:@selector(computingLoop) withObject:NULL];
+        
+        [self setUserInteractionEnabled:YES];
     }
     return self;
 }
@@ -71,6 +73,17 @@
             CGContextFillRect(context, CGRectMake(x-0.5, 0, 1, self.bounds.size.height));
         }
         
+        // draw the selected output frequency
+        if ([[AAAudioManager manager] outputEnabled]) {
+            float focusedFrequency = [[[AAAudioManager manager] signalGenerator] frequency];
+            if (focusedFrequency > 0) {
+                float xs = xFactor * (log2(focusedFrequency/5) - minFreqLog);
+                [[UIColor redColor] setFill];
+                CGContextFillRect(context, CGRectMake(xs-1.5, 0, 3, self.bounds.size.height));
+            }
+        }
+        
+        // draw the spectre
         CGContextBeginPath(context);
         for (int i = 0; i < tmp.count; i ++) {
             float xs = ([tmp[i][@"f"] doubleValue] == 0 ? 0 : xFactor * (log2([tmp[i][@"f"] doubleValue]/5) - minFreqLog));
@@ -91,6 +104,27 @@
 
 - (void) inputGotAudioBuffer: (void *)buffer size:(int)size {
     [fft appendData:buffer length:(int)size];
+}
+
+#pragma mark - User interaction
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self selectFrequencyFromTouch:[touches anyObject]];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self selectFrequencyFromTouch:[touches anyObject]];
+}
+
+- (void) selectFrequencyFromTouch:(UITouch *)touch {
+    CGPoint location = [touch locationInView:self];
+    float x = location.x;
+    // convert to frequency
+    float minFreqLog = log2(20/5);
+    float xFactor = self.bounds.size.width / (log2(20000/5) - minFreqLog);
+    // x = xFactor * (log2(frequency/5) - minFreqLog)
+    float frequency = pow(2, (x / xFactor) + minFreqLog)*5;
+    [[[AAAudioManager manager] signalGenerator] setFrequency:frequency];
 }
 
 @end
